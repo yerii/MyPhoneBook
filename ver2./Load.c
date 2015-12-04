da2a47c63551b8a45d5ebc06d8b5b26de146da8c
KangWoo Kim <rkarkd@naver.com>
2015-12-04 16:32:35 +0900

    Load func

---
+++ ver2./Load.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <memory.h>
+#include <locale.h>
+
+void Load()
+{
+	int i, cnt = 0, k, j = 0, overlap = 0;
+	int dataflag = 0;
+	int nameflg = 0, telflg = 0, emailflg = 0, noteflg = 0;
+
+	char *filename = (char *)malloc(80);
+
+}
