c49e28bdedcd27380b5cb52b0518e8d50d5585ad
KangWoo Kim <rkarkd@naver.com>
2015-12-05 19:53:20 +0900

    codeclear

--- addtoBST.c
+++ addtoBST.c
@@ -1,49 +1,34 @@
 void addToBST(char *name, char *phone, char *email, char *memo)
-{
-	struct node *new_node = createNewNode(name,phone, email,memo);
-
+{	struct node *new_node = createNewNode(name,phone, email,memo);
 	if(root==0)
 	{
 		root = new_node;
 		return;
 	}
 	else
-	{
-		struct node *cur_node=root;
+	{	struct node *cur_node=root;
 		while(1)
-		{
-		if(strcmp(cur_node->name, new_node->name)>0)
-		{
-			if(cur_node->left==0)
-			{
-				cur_node->left=new_node;
+		{if(strcmp(cur_node->name, new_node->name)>0)
+		{if(cur_node->left==0)
+			{cur_node->left=new_node;
 				return;
-			}
-			cur_node=cur_node->left;
+			}cur_node=cur_node->left;
 		}
 		else if(strcmp(cur_node->name,new-node->name)<0)
-		{
-			if(cur_node->right==0)
-			{
-				cur_node->right = new_node;
-				return;
-			}
+		{if(cur_node->right==0)
+			{cur_node->right = new_node;
+				return;}
 			cur_node = cur_node->left;
 		}
-
 		else if(strcmp(cur_node->name, new_node->name)<0)
 		{
 			if(cur_node->right==0)
-			{
-				cur_node->right=new_node;
-				return;
-			}
+			{cur_node->right=new_node;
+				return;}
 			cur_node=cur_node->right;
 		}
 		else
-		{
-			printf("same name");
-		}
+		{printf("same name");}
 	}
 	}
 }
